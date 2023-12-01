98991ab64b7ad863b6a5438126d9930e7fc93604
Alec Mirambeau <alecmirambeau@gmail.com>
2023-12-01 13:44:27 -0500

    Added 2 new functions and 2 new unit tests

--- math_functions.py
+++ math_functions.py
@@ -10,7 +10,14 @@ def multiply_numbers(a, b):
 def divide_numbers(a, b):
     return a / b
 
+def floor_division(a, b):
+    return a // b
+
+def power_numbers(a, b):
+    return a ** b
+
 if __name__ == "__main__":
     print("Adding:", add_numbers(2,4))
     print("Subtracting:", subtract_numbers(9,2))
+    print(divide_numbers(9, 2))
 
--- test_math_functions.py
+++ test_math_functions.py
@@ -16,6 +16,12 @@ def test_calc_multiply():
 #    output = multiply_numbers(2,4)
 #    assert output == 16
 
+def test_calc_power():
+    assert power_numbers(2, 3) == 8
+
+def test_floor_division():
+    assert floor_division(9, 2) == 4
+
 def test_calc_divide():
     output = divide_numbers(10,2)
     assert output == 5
